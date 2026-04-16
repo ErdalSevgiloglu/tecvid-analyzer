ea3e0422cea3b7781c810d86616e6f45dc9d212a
Erdal <erdalsevgiloglu@icloud.com>
2026-04-16 11:39:45 +0300

    fix: remove scipy dependency

--- tecvid_backend.py
+++ tecvid_backend.py
@@ -7,8 +7,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import librosa
 import numpy as np
-from scipy import signal
-from scipy.fft import fft
 import os
 import tempfile
 
